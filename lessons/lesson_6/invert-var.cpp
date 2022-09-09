7ff41ef44eb2dd8a04c51c8caf473ca0c968b0e6
Mateus <creztfallen@M.S.M>
2022-09-09 14:17:40 -0300

    goto

---
+++ lessons/lesson_6/invert-var.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int num = 10;
+    cout << num << endl;
+
+    // num = num * -1;
+    // cout << -num << endl;  it doesn't save the negative value
+
+    num = -num;
+    cout << num << endl;
+
+    return 0;
+}
